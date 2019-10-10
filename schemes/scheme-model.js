a17360f9c2883071d30b13a527b91725cf48c434
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-10-10 02:07:18 -0400

    create helper functions

---
+++ schemes/scheme-model.js
@@ -0,0 +1,40 @@
+const knex = require("knex")
+const knexConfig = require("../knexfile")
+
+const db = knex(knexConfig.development)
+
+module.exports = { find, findById, findSteps, add, update, remove }
+
+function find() {
+  return db("schemes")
+}
+
+function findById(id) {
+  return db("schemes")
+    .select("*")
+    .where({ id })
+}
+
+function findSteps(id) {
+  return db("steps")
+    .select("scheme_name", "step_number", "instructions")
+    .where({ id })
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .returning("*")
+}
+
+function update(id, changes) {
+  return db("schemes")
+    .update(changes)
+    .where({ id })
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del()
+}
